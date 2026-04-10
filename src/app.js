f8c43e78320edc5d8e149e7740957242f3ab2ed2
Juan Esteban Sosa Martinez <juan.sosa@estudiantesunibague.edu.co>
2026-04-10 16:39:16 -0500

    fix cors definitivo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from "express";
+import cors from "cors"; // 👈 IMPORTANTE
 import productRoutes from "./infrastructure/routes/productRoutes.js";
-import { productController } from "./infrastructure/config/container.js";
-import cors from "cors";
-app.use(cors());
 
 const app = express();
 
+app.use(cors()); // 👈 ESTA ES LA CLAVE
 app.use(express.json());
 
 app.use("/products", productRoutes);
