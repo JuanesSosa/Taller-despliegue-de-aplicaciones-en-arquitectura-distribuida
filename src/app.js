1e3d497d869db0d443e29c409aadd68b3151f49f
Juan Esteban Sosa Martinez <juan.sosa@estudiantesunibague.edu.co>
2026-04-10 16:35:26 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import productRoutes from "./infrastructure/routes/productRoutes.js";
 import { productController } from "./infrastructure/config/container.js";
+import cors from "cors";
+app.use(cors());
 
 const app = express();
 
